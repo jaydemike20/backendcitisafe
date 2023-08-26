8907ab3341a50cdf819e60b2f4cb31fe54fa5b48
jaydemike21 <jaydemike21@gmail.com>
2023-08-26 18:03:34 +0800

    Added new branch for drivers and create initial models for driver

---
+++ project/drivers/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+# Create your models here.
+class Classification():
+    class_type = models.CharField(max_length=5)
+    
+class Agency():
+    agency_code = models.CharField(max_length=255)
+
+class Driver(models.Model):
+
+    GENDER_CHOICES = [
+    ('M', 'Male'),
+    ('F', 'Female'),
+    ('O', 'Other'),
+    ]
+    
+    license_number = models.CharField(max_length=15, null=False, blank=False)
+    first_name = models.CharField(max_length=100)
+    middle_initial = models.CharField(max_length=1)
+    last_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+    birthdate = models.DateField()
+    nationality = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    contact_number = models.CharField(max_length=50)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, null=True, blank=True)
+    weight = models.DecimalField(max_digits=5, decimal_places=2)
+    height = models.DecimalField(max_digits=4, decimal_places=2)
+    expiration_date = models.DateField()
+    blood_type = models.CharField(max_length=3)
+    eyes_color = models.CharField(max_length=20)
+    dl_codes = models.CharField(max_length=100)
+    condition = models.CharField(max_length=10)
+    
