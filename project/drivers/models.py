209f2705a4fdf30e7ad0894be3bbaa5750dbf186
jaydemike <jaydemike21@gmail.com>
2023-08-27 13:36:14 +0800

    Create Model for Drivers

--- project/drivers/models.py
+++ project/drivers/models.py
@@ -3,21 +3,24 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-# Create your models here.
-class Classification():
+class Classification(models.Model):
     class_type = models.CharField(max_length=5)
     
-class Agency():
+class Agency(models.Model):
     agency_code = models.CharField(max_length=255)
 
 class Driver(models.Model):
 
     GENDER_CHOICES = [
-    ('M', 'Male'),
-    ('F', 'Female'),
-    ('O', 'Other'),
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
     ]
     
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    agency = models.OneToOneField(Agency, on_delete=models.CASCADE)
+    classification = models.OneToOneField(Classification, on_delete=models.CASCADE)
+
     license_number = models.CharField(max_length=15, null=False, blank=False)
     first_name = models.CharField(max_length=100)
     middle_initial = models.CharField(max_length=1)
@@ -35,4 +38,3 @@ class Driver(models.Model):
     eyes_color = models.CharField(max_length=20)
     dl_codes = models.CharField(max_length=100)
     condition = models.CharField(max_length=10)
-    
