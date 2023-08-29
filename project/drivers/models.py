98794fa44522552696536ae3ee31e4b90ae7fbc5
jaydemike21 <jaydemike21@gmail.com>
2023-08-29 21:24:47 +0800

    Created Vehicle Model

--- project/drivers/models.py
+++ project/drivers/models.py
@@ -21,14 +21,17 @@ class Driver(models.Model):
     classification = models.ForeignKey(Classification, on_delete=models.CASCADE)
 
     license_number = models.CharField(max_length=15, null=False, blank=False, unique=True)
+    # full name
     first_name = models.CharField(max_length=100)
     middle_initial = models.CharField(max_length=1)
     last_name = models.CharField(max_length=100)
+
     address = models.CharField(max_length=255)
     birthdate = models.DateField()
     contact_number = models.CharField(max_length=50)
 
     nationality = models.CharField(max_length=100, null=True, blank=True)
+    
     email = models.CharField(max_length=100, null=True, blank=True)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, null=True, blank=True)
     weight = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
@@ -37,7 +40,6 @@ class Driver(models.Model):
     expiration_date = models.DateField()
     blood_type = models.CharField(max_length=3, null=True, blank=True)
     eyes_color = models.CharField(max_length=20, null=True, blank=True)
-
     agency_code = models.CharField(max_length=255)
     dl_codes = models.CharField(max_length=100)
     condition = models.CharField(max_length=10)
