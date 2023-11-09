7b627d948518139b71ea110bcbc7c542e9ca91fc
jaydemike21 <jaydemike21@gmail.com>
2023-11-09 22:03:31 +0800

    Some changes

--- project/ticket/views.py
+++ project/ticket/views.py
@@ -10,20 +10,20 @@ from rest_framework.permissions import IsAuthenticated
 class penaltyListCreateAPIView(ListCreateAPIView):
     serializer_class = penaltySerializers
     queryset = penalty.objects.all()
-    permission_classes = [IsAuthenticated & (AdminPermission)]
+    # permission_classes = [IsAuthenticated & (AdminPermission)]
 
 
 class penaltyRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     serializer_class = penaltySerializers
     queryset = penalty.objects.all()
-    permission_classes = [IsAuthenticated & (AdminPermission)]
+    # permission_classes = [IsAuthenticated & (AdminPermission)]
     
 
 
 class violationListCreateAPIView(ListCreateAPIView):
     serializer_class = violationSerializer
     queryset = violation.objects.all()
-    permission_classes = [IsAuthenticated & (AdminPermission)]
+    # permission_classes = [IsAuthenticated & (AdminPermission)]
 
 
 class violationRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
@@ -36,7 +36,7 @@ class violationRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
 class trafficviolationListCreateAPIView(ListCreateAPIView):
     serializer_class = traffic_violationSerializer
     queryset = traffic_violation.objects.all()
-    permission_classes = [IsAuthenticated & (AdminPermission)]
+    # permission_classes = [IsAuthenticated & (AdminPermission)]
 
 
 class trafficviolationRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
@@ -48,7 +48,7 @@ class trafficviolationRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView)
 class ticketListCreateAPIView(ListCreateAPIView):
     serializer_class = ticketSerializer
     queryset = ticket.objects.all()
-    permission_classes = [IsAuthenticated & (EnforcerPermission)]
+    # permission_classes = [IsAuthenticated & (EnforcerPermission)]
 
     # def get_queryset(self):
     #     # Filter traffic tickets based on the logged-in officer
